27fa1b22de0c253117decafe8358d935044b40d3
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2021-09-23 12:12:44 +0200

    Update modal.js

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,23 +1,27 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// DOM ELEMENTS MODAL 
 
-// DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Button Close Modal
+const closeBtn = document.querySelector('.close');
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+
+        //==============DISPLAY MODAL=============//
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form 
+const closeModal = () => {
+  modalbg.style.display ='none';
+}
 
+// close modal event 
+closeBtn.addEventListener('click', (closeModal));
